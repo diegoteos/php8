df24036d4fe724f255dfc506b3d0803071f954f5
diegoteos <diegoteos@hotmail.com>
2022-11-30 22:27:22 -0600

    foreach

--- php/for.php
+++ php/for.php
@@ -7,4 +7,22 @@ for ($i = 0; $i <= $filas; $i++) {
     for ($j = 1; $j <= $i; $j++) {
         echo "*";
     }
+    echo "<br/>";
 }
+
+echo "<br/>";
+echo "<br/>";
+
+$array = array(
+    'vaca',
+    'burro',
+    'jirafa',
+    'elefante',
+    'castor',
+
+);
+
+foreach ($array as $animal){
+    echo "$animal <br/>";
+}
+
