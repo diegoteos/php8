9e42412aab434e4988b8f9c7d148baac6a552350
diegoteos <diegoteos@hotmail.com>
2022-12-03 21:45:27 -0600

    die exit break continue
    Para detener la ejecucion del programa

--- php/for.php
+++ php/for.php
@@ -5,12 +5,11 @@ $filas = 10;
 
 for ($i = 0; $i <= $filas; $i++) {
     for ($j = 1; $j <= $i; $j++) {
-        echo "*";
+       // echo "*";
     }
-    echo "<br/>";
+    //echo "<br/>";
 }
 
-echo "<br/>";
 echo "<br/>";
 
 $array = array(
@@ -19,10 +18,51 @@ $array = array(
     'jirafa',
     'elefante',
     'castor',
-
+    
 );
 
 foreach ($array as $animal){
-    echo "$animal <br/>";
+    // echo "$animal <br/>";
 }
 
+
+// parar una ciclo al cumplir con una condicion
+
+for ($i = 0; $i <= 20; $i++){
+    
+    echo "$i <br/>";
+    
+    if ($i == 10){
+        break;
+    }
+}
+
+echo "<br/>";
+
+// continue permite saltarse un ciclo
+
+for ($i = 0; $i <= 20; $i++){
+    
+    if ($i == 10){
+        echo "Este ciclo se omitio <br/>";
+        continue;
+    }
+
+    echo "$i <br/>";
+}
+
+
+// con las funciones die(); o exit(); detenemos completamente la ejecucion del programa
+
+for ($i = 0; $i <= 20; $i++){
+    
+    if ($i == 10){
+        echo "Detenemos la ejecucion <br/>";
+        exit();
+        //die();
+    }
+
+    echo "$i <br/>";
+}
+
+echo "hemos cumplido con todo";
