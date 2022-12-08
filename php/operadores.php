b4d65c0328ac80bc8aef991229c7d95821a6b987
diegoteos <diegoteos@hotmail.com>
2022-12-07 18:52:48 -0600

    operadores logicos

---
+++ php/operadores.php
@@ -0,0 +1,21 @@
+$a and $b	And (y)	true si tanto $a como $b son true. <br>
+$a or $b	Or (o inclusivo)	true si cualquiera de $a o $b es true. <br>
+$a xor $b	Xor (o exclusivo)	true si $a o $b es true, pero no ambos. <br>
+! $a	Not (no)	true si $a no es true. <br>
+$a && $b	And (y)	true si tanto $a como $b son true. <br>
+$a || $b	Or (o inclusivo)	true si cualquiera de $a o $b es true. <br>
+
+<?php
+
+$a = true;
+$b = true;
+$c = false;
+$d = false;
+
+$resultado = $a && $b;
+$resultado2 = $c && $d;
+$resultado2 = $c && $d;
+
+var_dump($resultado);
+echo "<br/>";
+var_dump($resultado2);
