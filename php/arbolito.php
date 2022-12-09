e3c453d340b19081509526486329d860af7cc85d
diegoteos <diegoteos@hotmail.com>
2022-12-09 16:59:32 -0600

    arbolito

--- php/arbolito.php
+++ php/arbolito.php
@@ -2,6 +2,17 @@
 
 $asterisco = '*';
 
-for ($i = 0; $i <= 5; $i++) {
+for ($i = 0; $i <= 10; $i++) {
     echo str_repeat($asterisco, $i) . '<br />';
 }
+
+echo '<br />';
+echo '<br />';
+echo '<br />';
+
+for ($i = 0; $i <= 10; $i++) {
+    $espacio = '&nbsp';
+    echo str_repeat($espacio, 10 - $i);
+    echo str_repeat($asterisco, $i) . '<br />';
+}
+
