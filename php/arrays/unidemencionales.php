a4360a53f1009a9944a338b92b5af723180a1cc3
diegoteos <diegoteos@hotmail.com>
2022-12-11 22:49:01 -0600

    arreglos bidimencionales

--- php/arrays/unidemencionales.php
+++ php/arrays/unidemencionales.php
@@ -4,5 +4,28 @@ $array = [19, 12, 13, 29];
 
 $array[] = 18;
 
-
 echo $array[4];
+
+echo '<br/>';
+
+$data = [
+
+    [
+        'nombre' => 'Victor Flores',
+        'email' => 'victor@gmail.com',
+        'celular' => '22332322'
+    ],
+    [
+        'nombre' => 'maria perez',
+        'email' => 'maria@gmail.com',
+        'celular' => '22311322'
+    ],
+    [
+        'nombre' => 'Casimira argueta',
+        'email' => 'casimira@gmail.com',
+        'celular' => '22209322'
+    ],
+
+];
+
+echo $data[0]['nombre'];
