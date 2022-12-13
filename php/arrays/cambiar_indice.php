6175167e2477d88e3fbe3154eff10c0358947ca5
diegoteos <diegoteos@hotmail.com>
2022-12-12 22:59:50 -0600

    arrays

--- php/arrays/cambiar_indice.php
+++ php/arrays/cambiar_indice.php
@@ -8,6 +8,48 @@ $nombres = array(
     'mario'
 );
 
+// le asignamos nuevo indice a nuestro array
 list($z, $a, $b, $c, $d) = $nombres;
 
 echo $a;
+
+echo '<br/><br/>';
+
+$llenar = range(1, 10);
+
+var_dump($llenar);
+
+echo '<br/><br/>';
+
+echo count($llenar);
+
+
+echo '<br/><br/>';
+
+// buscamos un valor dentro de un array
+
+$buscar = 'mario';
+if ($nombre = in_array($buscar, $nombres)) {
+    $posicion = array_search($buscar, $nombres);
+    echo 'si existe el nombre: ' . $nombres[$posicion] . ' en el array de nombres en la posicion: ' . $posicion;
+} else {
+    echo "El valor buscado no se encuentra";
+}
+
+echo '<br/><br/>';
+
+// eliminamos un valor de un array
+
+unset($nombres[4]);
+
+echo '<br/><br/>';
+
+// repetimos la busqueda
+$buscar = 'mario';
+if ($nombre = in_array($buscar, $nombres)) {
+    $posicion = array_search($buscar, $nombres);
+    echo 'si existe el nombre: ' . $nombres[$posicion] . ' en el array de nombres en la posicion: ' . $posicion;
+} else {
+    echo "El valor buscado no se encuentra";
+}
+
