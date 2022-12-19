7cf7e91459b7e28774221b33c574a1a604c3cfa1
diegoteos <diegoteos@hotmail.com>
2022-12-18 21:35:01 -0600

    buscar

--- php/string/str_search.php
+++ php/string/str_search.php
@@ -7,5 +7,16 @@ echo '<br/>';
 echo '<br/>';
 echo '<br/>';
 
-$dos = search($hola, $dos);
+// obtenemos la posicion de la palabra buscada
+echo $dos = strpos($hola, 'mundo');
 
+
+echo '<br/>';
+
+$email = 'guatemala@ejemplo.com';
+  echo stristr($email, 'u');
+
+echo '<br/>';
+
+$texto = 'El mundo ha venido siendo lo que notros hemos hecho de el.';
+  echo strpbrk($texto, 'mundo');
