a22d75485135d9aae647a8b596a1416127a9703d
diegoteos <diegoteos@hotmail.com>
2022-12-19 18:35:24 -0600

    consultas sql

--- php/conexion_sql/consulta.php
+++ php/conexion_sql/consulta.php
@@ -1,2 +1,57 @@
 <?php
 
+include_once 'conexion.php';
+
+$con = conexion();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Personas vacunadas</title>
+</head>
+
+<body>
+    <br>
+    <div class="container">
+    <h1>Consulta personas:</h1>
+    <h3>Listado</h3>
+
+        <table class="table table-striped">
+            <tr>
+                <th>Nombre</th>
+                <th>DPI</th>
+            </tr>
+            <?php
+            // $primerNombre = 'aaron';
+            // $segundoNombre = 'aaron';
+            // // $tercerNombre = 'aaron';
+            // $primerApellido = 'aaron';
+            // $segundoApellido = 'aaron';
+
+            $sql = "SELECT * FROM `vacunas` WHERE `nombres` LIKE '%aaron%' AND `nombres` LIKE '%teos%'";
+            $query = mysqli_query($con, $sql);
+            if ($query) {
+                while ($row = mysqli_fetch_array($query)) {
+                    $nombre = $row['nombres'];
+                    $dpi = $row['dpi'];
+            ?>
+                    <tr>
+                        <td><?php echo $nombre; ?></td>
+                        <td><?php echo $dpi; ?></td>
+                    </tr>
+            <?php
+                }
+            }
+            ?>
+        </table>
+    </div>
+</body>
+
+</html>
