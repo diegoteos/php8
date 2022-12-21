4d25d4d3002d6d32a5cd748a89c9f1af685f3a47
diegoteos <diegoteos@hotmail.com>
2022-12-20 21:41:27 -0600

    archivos

--- php/archivos/procesar.php
+++ php/archivos/procesar.php
@@ -2,6 +2,13 @@
 $nombre = $_REQUEST['name'];
 $image = $_FILES['image'];
 
+$patch = $_SERVER['DOCUMENT_ROOT'] . '/php8/php/archivos/imagenes/' . $image['name'];
 
 echo $nombre . '<br /><br>';
 var_dump($image);
+
+echo '<br /><br>';
+
+//echo $patch;
+
+move_uploaded_file($image['tmp_name'], $patch);
