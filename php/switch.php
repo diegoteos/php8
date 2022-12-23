8b4468087e07da8bef6afa7db2ccd6cf9dc2b326
diegoteos <diegoteos@hotmail.com>
2022-12-22 23:14:55 -0600

    switc

---
+++ php/switch.php
@@ -0,0 +1,15 @@
+<?php
+
+$hola = 1;
+
+switch ($hola) {
+    case 0:
+        echo "el valor es cero";
+        break;
+    case 1:
+        echo "el valor es uno";
+        break;
+    default:
+        echo "no es ningun valor valido";
+        break;
+}
