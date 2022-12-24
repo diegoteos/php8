a8ff7a34eea30897a3e62b96d4a5d2c397c72056
diegoteos <diegoteos@hotmail.com>
2022-12-23 19:13:54 -0600

    poo

---
+++ php/poo/clases.php/personas.php
@@ -0,0 +1,19 @@
+<?php
+
+// creamos una clase
+
+class Persona
+{
+
+    // los objetos tienen propiedades y metodos
+
+    // propiedades
+
+    public $nombre, $apellido, $edad; // se puede dejar en una sola linea o una propiedad en cada linea
+
+    
+
+
+
+
+}
