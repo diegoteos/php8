c103aa5ab92e6434359caa31c85e95f23398f622
diegoteos <diegoteos@hotmail.com>
2022-12-23 19:57:51 -0600

    poo

---
+++ php/poo/clases/Persona.php
@@ -0,0 +1,36 @@
+<?php
+
+// creamos una clase
+
+class Persona
+{
+    // propiedades (variables)
+    public $nombre, $apellido, $edad; // se puede dejar en una sola linea o una propiedad en cada linea
+
+    // constructor el cual se ejecuta cuando instanciamos
+
+    public function __construct($nombre, $apellido, $edad){
+        $this->nombre = strtolower($nombre);
+        $this->apellido = strtolower($apellido);
+        $this->edad = $edad;
+    }
+
+    // los objetos tienen propiedades y metodos
+
+
+
+    // metodos (funciones)
+
+    public function setNombre($nombre){
+        $this->nombre = strtolower($nombre);
+    }
+
+    public function getNombre(){
+        return ucwords($this->nombre);    
+    }
+    
+
+
+
+
+}
--- php/poo/index.php
+++ php/poo/index.php
@@ -1,6 +1,31 @@
 <?php
 
-require_once 'clases/personas.php';
+require_once 'clases/Persona.php';
+
+$persona1 = new Persona("Victor Hernandez", "Arana Flores", 22);
+
+// $persona1->setNombre("Diego Teos");
+// $persona1->apellido = "Arana Flores";
+// $persona1->edad = 35;
+
+
+$persona2 = new Persona("Maria Magdalena", "Gonzalez", 22);
+// $persona2->setNombre("Lucia Maria");
+// $persona2->apellido = "Fuentes Lopez";
+// $persona2->edad = 19;
+
+echo '<pre>';
+var_dump($persona1);
+echo '<br>';
+var_dump($persona2);
+
+echo '<br><br>';
+
+echo "El nombre de la persona 1 es: " . $persona1->getNombre();
+echo '<br>';
+echo "El nombre de la persona 2 es: " . $persona2->getNombre();
+
+
 
 
 
