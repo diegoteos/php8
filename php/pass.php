e1292788b8ea86421592f97316d271b9e002c6b7
diegoteos <diegoteos@hotmail.com>
2022-12-26 18:23:23 -0600

    password_hash default

--- php/pass.php
+++ php/pass.php
@@ -1,6 +1,22 @@
 <?php
 
-echo "hola mundo: ";
+
+$contraseña = "Hola mundo";
+
+$nuevaContraseña = password_hash($contraseña, PASSWORD_DEFAULT);
+
+echo $nuevaContraseña;
+echo '<br /><br /><br /><br /><br /><br>';
+
+$hash = '$2y$10$YVcy3t4Mp6yJnA7yYHtCQe9qgwud0QfMP7nB3HvZtlkHMkl.Kg/R.';
+
+
+if (password_verify($contraseña, $hash)){
+echo "Si son igualitas: ";
+} else {
+    echo 'La contraseña no es válida.';
+}
+
 
 
 
